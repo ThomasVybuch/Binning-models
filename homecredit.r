45d7641b85e2ff0767696a3fed699ce8665b1823
hajkovel <126002539+hajkovel@users.noreply.github.com>
2023-10-30 11:46:29 +0100

    Update homecredit.r

--- homecredit.r
+++ homecredit.r
@@ -18,3 +18,32 @@ data=data[!grepl(365243, data$DAYS_EMPLOYED),]               # removed about 50K
 summary(data)
 boxplot(data$YEARS_EMPLOYED)
 hist(data$YEARS_EMPLOYED)
+
+
+
+
+###### Prvni pokusy SMBINNING
+#install.packages("smbinning")
+library(smbinning)
+
+?smbinning
+(result=smbinning(data, "TARGET", "AMT_INCOME_TOTAL"))
+
+par ( mfrow= c ( 2 , 2 ) )  
+boxplot ( data$ AMT_INCOME_TOTAL ~ data $ TARGET ,  
+          horizontal=T ,  frame =F ,  col = "lightgrey" , main= "Distribution " )  
+mtext ( "A" , 3 )  
+smbinning.plot ( result , option= "dist" )  
+smbinning.plot ( result , option= "badrate")  
+smbinning.plot ( result , option= "WoE")
+
+##WoE
+#install.packages("woe")
+library(woe)
+
+
+#Logistická regrese 
+M_1 = glm (TARGET ~ CODE_GENDER+FLAG_OWN_REALTY+
+           AMT_INCOME_TOTAL+AMT_CREDIT+AMT_ANNUITY+NAME_FAMILY_STATUS+
+           DAYS_BIRTH+DAYS_EMPLOYED+NAME_EDUCATION_TYPE+NAME_HOUSING_TYPE, data = data, family = binomial)
+summary(M_1)
