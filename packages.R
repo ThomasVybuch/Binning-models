1e01f07708b7707ba7fbdd73a204dd41b888a3c1
ThomasVybuch <149273075+ThomasVybuch@users.noreply.github.com>
2023-12-12 22:47:48 +0100

    Add files via upload

---
+++ packages.R
@@ -0,0 +1,10 @@
+# List of all necessary packages
+install.packages("here")
+install.packages("smbinning")
+install.packages("woe")
+install.packages("rpart")
+install.packages("rpart.plot")
+#install.packages("randomForest")
+
+
+
